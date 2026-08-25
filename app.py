26284b3e8fa0acab26a7a96040faf6d35401dc5f
MeetMude <meetmude@gmail.com>
2026-08-25 14:35:01 +0530

    Bind to 0.0.0.0 and use Render's assigned PORT

--- app.py
+++ app.py
@@ -320,4 +320,6 @@ def download(filename):
     return send_from_directory(GENERATED_DIR, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
